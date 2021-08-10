24f568df6a4e3df21474f5e2333450f2bfe10d3c
Scott McDonald <scottmcdonald@Scotts-MacBook-Air.local>
2021-08-10 08:10:10 -0500

    Adjusted JS to fire when window loads

--- script/index.js
+++ script/index.js
@@ -1,28 +1,31 @@
-let userDropdown = document.getElementById('user-info');
-let dropdownMenu = document.querySelector('.nav-dropdown');
 
-userDropdown.addEventListener('click', () => {
-    if (dropdownMenu.classList.contains('active')) {
-        dropdownMenu.classList.remove('active');
-    } else {
-        dropdownMenu.classList.add('active');
-    }
-})
+window.addEventListener('load', () => {
+     let userDropdown = document.getElementById('user-info');
+    let dropdownMenu = document.querySelector('.nav-dropdown');
 
-let panels = document.querySelectorAll('.panel');
-
-panels.forEach(panel => {
-    panel.addEventListener('mouseover', () => {
-        if(!panel.classList.contains('open')) {
-            panel.classList.add('open');
-            // panel.style.animation = 'book-open 500ms forwards'
+    userDropdown.addEventListener('click', () => {
+        if (dropdownMenu.classList.contains('active')) {
+            dropdownMenu.classList.remove('active');
+        } else {
+            dropdownMenu.classList.add('active');
         }
     })
-    panel.addEventListener('mouseout', () => {
-        if(panel.classList.contains('open')) {
-            panel.classList.remove('open');
-            // panel.style.animation = 'book-open 500ms reverse forwards'
-        }
+
+    let panels = document.querySelectorAll('.panel');
+
+    panels.forEach(panel => {
+        panel.addEventListener('mouseover', () => {
+            if(!panel.classList.contains('open')) {
+                panel.classList.add('open');
+                // panel.style.animation = 'book-open 500ms forwards'
+            }
+        })
+        panel.addEventListener('mouseout', () => {
+            if(panel.classList.contains('open')) {
+                panel.classList.remove('open');
+                // panel.style.animation = 'book-open 500ms reverse forwards'
+            }
+        })
     })
 })
 
